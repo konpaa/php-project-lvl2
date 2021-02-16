f1edc3a359c77b5c3096d338e6494c1440e34294
Konpa <pavelkonoplyanikov@yandex.by>
2021-02-16 11:14:14 +0300

    fix

--- src/Parsers.php
+++ src/Parsers.php
@@ -2,15 +2,15 @@
 
 namespace Php\Project\Lvl2\Parsers;
 
-use function Php\Project\Lvl2\Parsers\parseJson;
-use function Php\Project\Lvl2\Parsers\parseYaml;
+use function Php\Project\Lvl2\Parsers\parseJson as Json;
+use function Php\Project\Lvl2\Parsers\parseYam as Yaml;
 
 function parseData(string $data, string $parserType): object
 {
     $parsers = [
-        'json' => fn($data) => parseJson($data),
-        'yaml' => fn($data) => parseYaml($data),
-        'yml' => fn($data) => parseYaml($data)
+        'json' => fn($data) => Json($data),
+        'yaml' => fn($data) => Yaml($data),
+        'yml' => fn($data) => Yaml($data)
     ];
 
     if (!array_key_exists($parserType, $parsers)) {
